bb54b4c92deb448c1390954ac86700b63d17b8b2
ElafAhmad <elafalamri7@gmail.com>
2019-07-18 10:44:57 +0300

    Update apple.sol
    Finished update status function and linked it to Add application function to automate the accept/reject

--- DiggBox/contracts/apple.sol
+++ DiggBox/contracts/apple.sol
@@ -11,6 +11,9 @@ string location;
 uint depth;
 uint length;
 uint cost;
+
+string startDate;
+string endDate;
 bool status;
 
 }
@@ -19,17 +22,26 @@ Apple[20] public apples;
 
 mapping (address => uint) public AppleAdress;
 
- function application(string memory name, string memory location, uint depth, uint length, uint cost) public {
-        apples[id] = Apple(id, msg.sender,name, location, depth, length, cost, false);
+ function application(string memory name, string memory location, uint depth, uint length, uint cost,
+  string memory sdate, string memory edate) public {
+        apples[id] = Apple(id, msg.sender,name, location, depth, length, cost, sdate, edate, false);
        AppleAdress[msg.sender] = id;
+       setstatues(id);
         id++;
-    }
+        }
 
     function setstatues (uint ApplicationID) public {
-       // require(apples[ApplicationID].location = "Madinah","Location must be Madinah");
-       string memory x = "Madinah";
-       string memory y = apples[ApplicationID].location;
-        require(x = y, "error");
+        uint orgCount = 0;
+        require(keccak256(abi.encodePacked(apples[ApplicationID].location)) == keccak256(abi.encodePacked(("Madinah"))),"Location not Madinah");
+        require(apples[ApplicationID].length < 80, "Length bigger than 80");
+        require(apples[ApplicationID].depth < 80, "Depth bigger than 80");
+    
+        for (uint i = 0; i < apples.length; i++){
+       if (keccak256(abi.encodePacked(apples[ApplicationID].startDate)) == keccak256(abi.encodePacked((apples[i].startDate)))){
+           orgCount++;
+       }
+        }
+        require(orgCount <= 2,"More than 2 applications start at same date");
         apples[ApplicationID].status = true;
     }
     
