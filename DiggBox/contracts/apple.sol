6f9e896f6ba15031cd8f4b244f10844ecc6e6f6d
ElafAhmad <elafalamri7@gmail.com>
2019-07-18 03:02:58 +0300

    Update apple.sol
    setstatues Function

--- DiggBox/contracts/apple.sol
+++ DiggBox/contracts/apple.sol
@@ -11,6 +11,8 @@ string location;
 uint depth;
 uint length;
 uint cost;
+bool status;
+
 }
 
 Apple[20] public apples;
@@ -18,9 +20,17 @@ Apple[20] public apples;
 mapping (address => uint) public AppleAdress;
 
  function application(string memory name, string memory location, uint depth, uint length, uint cost) public {
-        apples[id] = Apple(id, msg.sender,name, location, depth, length, cost);
+        apples[id] = Apple(id, msg.sender,name, location, depth, length, cost, false);
        AppleAdress[msg.sender] = id;
         id++;
+    }
+
+    function setstatues (uint ApplicationID) public {
+       // require(apples[ApplicationID].location = "Madinah","Location must be Madinah");
+       string memory x = "Madinah";
+       string memory y = apples[ApplicationID].location;
+        require(x = y, "error");
+        apples[ApplicationID].status = true;
     }
     
    
